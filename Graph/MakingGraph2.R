6e6c13f1db55b2954618aeafe436f541754269d4
SamanthaLynn99 <59545840+SamanthaLynn99@users.noreply.github.com>
2020-03-04 11:04:39 -0500

    Create MakingGraph2.R
    Final Graph Code

---
+++ Graph/MakingGraph2.R
@@ -0,0 +1,29 @@
+source("TidyingData.R")
+
+library(ggplot2)
+str(tidy_race2)
+
+
+table(tidy_race2$Year)
+
+ggplot(tidy_race2,
+       aes(x=Year, y= Attendance, fill=Track_Conditions))+
+  geom_bar(stat="identity",
+           position="dodge")+
+  facet_wrap(~Location)
+  
+ggplot(tidy_race2,
+       aes(x=Year, y= Attendance, fill=Location))+
+  geom_bar(stat="identity",
+           position="dodge")+
+  facet_wrap(~Track_Conditions)
+
+#basically done Graph
+ggplot(tidy_race2,
+       aes(x=Year, y= Attendance, fill=Track_Conditions))+
+  geom_bar(stat="identity",
+           position="dodge")+
+  facet_wrap(~Location)+
+  scale_fill_manual(values=c("darkslategray3", "darkslategray2", "darkslategrey"))+
+  theme_minimal()
+
