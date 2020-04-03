78fcaac71e087f8c32eb601716081a1c44b3e179
sjkiss <sjkiss@gmail.com>
2020-04-03 10:09:46 -0400

    Comments for Katlyn Law

--- map_assignment/ckmap.R
+++ map_assignment/ckmap.R
@@ -26,4 +26,9 @@ ggplot(chathamkent4, aes(geomerty=geometry, fill=percent))+geom_sf()
 
 ggsave("chathamkent4.jpg")  
   
-  
+ggplot(chathamkent4, aes(geometry=geometry, fill=percent))+geom_sf()+labs(title="Percent of population that commutes outside of Chatham-Kent")+scale_fill_distiller(palette="Dark2")+theme_void()
+
+### Note that if you look in the help page for scale_fill_distiller, there are different sets of pre-made coloured palettes. Some are called sequential and some are called qualitative. There is a big difference. sequential palettes go step-by-step from a starting color to an end colour and they are useful when you are mapping a quantitative variable that has a lot of steps in between the lowest and the highest values. In this case, the qualitative category is al ittle better and the reason is that, although this is technically a quantitative variable (percent), there are actually clusters of people (e.g. commuters and non-commuters) in the differnt corners of the county. So, by showing this with a qualitative palette as opposed to a sequential palette, that pattern stands out more. 
+ggplot(chathamkent4, aes(geomerty=geometry, fill=percent))+geom_sf()+labs(title="Percent of population that commutes outside of Chatham-Kent, qualitative scale")+scale_fill_distiller(palette="Accent")+theme_void()
+ggplot(chathamkent4, aes(geomerty=geometry, fill=percent))+geom_sf()+labs(title="Percent of population that commutes outside of Chatham-Kent, sequential")+scale_fill_distiller(palette="Blues")+theme_void()
+
